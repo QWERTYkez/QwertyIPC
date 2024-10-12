892b9dace3eeeb7c996897dae46020f4a7a4d0b2
QWERTYkez <QWERTYkez@outlook.com>
2024-10-12 20:32:32 +0700

    Добавьте файлы проекта.

---
+++ SingleInstanceApp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace QwertyIPC;
+
+public static class SingleInstanceApp
+{
+    public class BySignal
+    {
+        public static bool IsFirstInstance(string appName, Action<CancellationTokenSource> onSignalReceived, out CancellationTokenSource cancelTokenSource)
+        {
+            cancelTokenSource = null!;
+            _ = new Mutex(true, $"{Environment.UserDomainName}-{Environment.UserName}-{appName}", out var prevInstance);
+            if (prevInstance)
+            {
+                cancelTokenSource = new CancellationTokenSource();
+                Transmitter.Signal.Receiver(appName, onSignalReceived, cancelTokenSource);
+            }
+            else
+            {
+                Transmitter.Signal.SendSignal(appName);
+            }
+            return prevInstance;
+        }
+        public static bool IsFirstInstance(string appName, Action onSignalReceived)
+        {
+            _ = new Mutex(true, $"{Environment.UserDomainName}-{Environment.UserName}-{appName}", out var prevInstance);
+            if (prevInstance)
+            {
+                Transmitter.Signal.Receiver(appName, onSignalReceived);
+            }
+            else
+            {
+                Transmitter.Signal.SendSignal(appName);
+            }
+            return prevInstance;
+        }
+    }
+
+    public class ByMessage
+    {
+        public static bool IsFirstInstance(string appName, string messageToFirstInstance, Action<string, CancellationTokenSource> onMessageReceived, out CancellationTokenSource cancelTokenSource)
+        {
+            cancelTokenSource = null!;
+            _ = new Mutex(true, $"{Environment.UserDomainName}-{Environment.UserName}-{appName}", out var prevInstance);
+            if (prevInstance)
+            {
+                cancelTokenSource = new CancellationTokenSource();
+                Transmitter.Message.Receiver(appName, onMessageReceived, cancelTokenSource);
+            }
+            else
+            {
+                Transmitter.Message.SendMessage(appName, messageToFirstInstance);
+            }
+            return prevInstance;
+        }
+        public static bool IsFirstInstance(string appName, string message, Action<string> onMessageReceived)
+        {
+            _ = new Mutex(true, $"{Environment.UserDomainName}-{Environment.UserName}-{appName}", out var prevInstance);
+            if (prevInstance)
+            {
+                Transmitter.Message.Receiver(appName, onMessageReceived);
+            }
+            else
+            {
+                Transmitter.Message.SendMessage(appName, message);
+            }
+            return prevInstance;
+        }
+    }
+}
---
+++ Transmitter.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QwertyIPC;
+
+public static class Transmitter
+{
+    public static class Signal
+    {
+        public static Task Receiver(string transmitterName, Action action) =>
+            Task.Run(() => ReceiverProcessing(transmitterName, action));
+        public static Task Receiver(string transmitterName, Action<CancellationTokenSource> action, CancellationTokenSource cancelTokenSource) =>
+            ReceiverProcessingAsync(transmitterName, action, cancelTokenSource);
+
+
+        static void ReceiverProcessing(string transmitterName, Action action)
+        {
+            while (true)
+            {
+                try
+                {
+                    using var server = new NamedPipeServerStream($"{Environment.UserDomainName}-{Environment.UserName}-{transmitterName}");
+                    server.WaitForConnection();
+                }
+                finally
+                {
+                    action?.Invoke();
+                }
+            }
+        }
+        static async Task ReceiverProcessingAsync(string transmitterName, Action<CancellationTokenSource> action, CancellationTokenSource cancelTokenSource)
+        {
+            while (true)
+            {
+                try
+                {
+                    using var server = new NamedPipeServerStream($"{Environment.UserDomainName}-{Environment.UserName}-{transmitterName}");
+                    await server.WaitForConnectionAsync(cancelTokenSource.Token);
+                    action?.Invoke(cancelTokenSource);
+                }
+                catch (OperationCanceledException)
+                {
+                    return;
+                }
+            }
+        }
+
+        public static void SendSignal(string pipeName)
+        {
+            using var client = new NamedPipeClientStream($"{Environment.UserDomainName}-{Environment.UserName}-{pipeName}");
+            client.Connect();
+            using var writer = new StreamWriter(client);
+        }
+    }
+
+    public static class Message
+    {
+        public static Task Receiver(string transmitterName, Action<string> action) =>
+            Task.Run(() => ReceiverProcessing(transmitterName, action));
+        public static Task Receiver(string transmitterName, Action<string, CancellationTokenSource> action, CancellationTokenSource cancelTokenSource) =>
+            ReceiverProcessingAsync(transmitterName, action, cancelTokenSource);
+
+
+        static void ReceiverProcessing(string transmitterName, Action<string> action)
+        {
+            while (true)
+            {
+                try
+                {
+                    using var server = new NamedPipeServerStream($"{Environment.UserDomainName}-{Environment.UserName}-{transmitterName}");
+                    server.WaitForConnection();
+                    var reader = new StreamReader(server);
+                    var message = reader.ReadToEnd();
+                    action?.Invoke(message);
+                }
+                catch
+                {
+                    return;
+                }
+            }
+        }
+        static async Task ReceiverProcessingAsync(string transmitterName, Action<string, CancellationTokenSource> action, CancellationTokenSource cancelTokenSource)
+        {
+            while (true)
+            {
+                try
+                {
+                    using var server = new NamedPipeServerStream($"{Environment.UserDomainName}-{Environment.UserName}-{transmitterName}");
+                    await server.WaitForConnectionAsync(cancelTokenSource.Token);
+                    var reader = new StreamReader(server);
+                    var message = reader.ReadToEnd();
+                    action?.Invoke(message, cancelTokenSource);
+                }
+                catch (OperationCanceledException)
+                {
+                    return;
+                }
+            }
+        }
+
+        public static void SendMessage(string pipeName, string message)
+        {
+            using var client = new NamedPipeClientStream($"{Environment.UserDomainName}-{Environment.UserName}-{pipeName}");
+            client.Connect();
+            using var writer = new StreamWriter(client);
+            writer.Write(message);
+        }
+    }
+}
